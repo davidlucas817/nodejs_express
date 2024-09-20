a892f868e31f6e02f5bc8a66e0f5742eb6b31447
David Lucas Cunha Moreira <142365156+davidlucas817@users.noreply.github.com>
2024-09-20 19:59:18 -0300

    Add files via upload

--- nodejs_express/sistema-loja/index.js
+++ nodejs_express/sistema-loja/index.js
@@ -1,49 +1,59 @@
-// Importando o Express na aplicação //
-const express = require("express") // CommonJS Modules (Node, forma antiga)
+const express = require("express") 
 
-// Criando uma instância do Express //
 const app = express()
 
-//Definindo o EJS como renderizador de páginas
 app.set(`view engine`, `ejs`)
 
-// Definir a pasta dos arquivos estáticos (public)
 app.use(express.static('public'))
 
-//ROTA PRINCIPAL
+//Principal
 app.get("/", (req, res) => {
-    //Será renderizada a página "index.ejs" que está na pasta 'views'
     res.render('index')
 })
 
-//ROTA PERFIL
-app.get("/perfil/:nome?", (req,res) => {
-    res.render('perfil')
+//Pedidos
+app.get("/pedidos", (req,res) => {
+    const listaPedidos = [
+        {id: "123", preco: "R$700,00", cpf: "348.122.455.69", status: "Saiu da transportadora"},
+        {id: "124", preco: "1200,00", cpf: "100.567.234.78", status: "Em preparação"},
+        {id: "125", preco: "R$149,99", cpf: "100.567.234.78", status: "Entregue"},
+        {id: "126", preco: "R$2799,99", cpf: "677.778.111.12", status: "Saiu da transportadora"}
+    ]
+    res.render("pedidos", {
+        listaPedidos: listaPedidos
+    })
 })
 
-//Criando a rota Videos
-app.get("/videos/:playlist?/:video?", (req,res) => {
-    res.render('videos')
+//Clientes
+app.get("/clientes", (req,res) => {
+    const cliente = [{nome: "Sonic Shopper", foto: "imgs/sonic1.jpg", cpf: '100.567.234.78', endereco: "Rua Eggman, nº5"},
+    {nome: "Comprando Sonics", foto: "imgs/sonic2.jpg", cpf: '348.122.455.69', endereco: "Rua Registro, nº8"},
+    {nome: "Sonic Sucesso", foto: "imgs/sonic3.jpg", cpf: '526.333.672.21', endereco: "Rua África, nº37"},
+    {nome: "Super Sonic", foto: "imgs/sonic4.jpg", cpf: '677.778.111.12', endereco: "Rua Japonês, nº1"},
+    ]
+    res.render('clientes',{
+        cliente : cliente,
+    })
 })
 
-// ROTA PRODUTOS
-app.get("/produtos/:produto?", (req,res) => {
-    const listaProdutos = ['Computador', 'Celular', 'Tablet', 'Notebook']
-    const produto = req.params.produto
+//Produtos
+app.get("/produtos", (req,res) => {
+    const produto = [{nome: "Eggman Toy + Mini Sonic", foto: "imgs/brinquedo1.jpg", preco: 'R$ 700,00', distrib: "Ebay Turkey"},
+        {nome: "Coleção Turma do Sonic Toy", foto: "imgs/brinquedo2.jpg", preco: 'R$ 1200,00', distrib: "Amazon Shopping"},
+        {nome: "Mini Tails Toy Ed. BK", foto: "imgs/brinquedo3.jpeg", preco: 'R$ 149,99', distrib: "Entregas Fáceis LTDA"},
+        {nome: "Pista do Sonic Toy", foto: "imgs/brinquedo4.jpg", preco: 'R$2799,99', distrib: "Entregas Fáceis LTDA"},
+    ]
     res.render('produtos',{
-        //Enviando a variável para a página
-        //Será chamado na página
-        produto : produto, //Variável que está na index (req.params)
-        listaProdutos : listaProdutos
-        //Na página produtos.ejs
+        produto : produto
     })
 })
 
 //Iniciando o servidor na porta 4000 //
-app.listen(4000, function(error) {
-    if(error){
-        console.log(`Ocorreu um erro: ${error}`)
-    }else{
-        console.log("Servidor iniciado com sucesso!")
-    }
-})
+const port = 4000;
+app.listen(port, (error) => {
+  if (error) {
+    console.log(`Ocorreu um erro: ${error}`);
+  } else {
+    console.log(`Servidor iniciado com sucesso em http://localhost:${port}`);
+  }
+});
